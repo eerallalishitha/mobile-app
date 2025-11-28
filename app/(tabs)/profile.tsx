916823249cd72bf768cff76b198f02bbaac920c8
Lishitha Eeralla <eerallalishitha@gmail.com>
2025-11-28 07:00:39 -0600

    Initial project upload

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+      <Text style={{ fontSize:22 }}>Profile Screen</Text>
+    </View>
+  );
+}
